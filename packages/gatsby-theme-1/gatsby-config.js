49acce400b36825edca2babad8920273d337971f
crossz <ukcrossa@gmail.com>
2022-05-15 23:51:55 +0800

    :tada: remove unnecessary gatsby plugins for gatsby 4.

--- packages/gatsby-theme-1/gatsby-config.js
+++ packages/gatsby-theme-1/gatsby-config.js
@@ -1,16 +1,5 @@
 module.exports = {
     plugins: [
-        {
-            resolve: `gatsby-plugin-create-client-paths`,
-            options: {
-                prefixes: [`/app/dashboard/*`]
-            }
-        },
-        {
-            resolve: `gatsby-plugin-page-creator`,
-            options: {
-                path: `${__dirname}/src/pages`
-            }
-        }
+
     ]
 }
